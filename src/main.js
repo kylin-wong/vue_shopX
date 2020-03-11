566e16c294049c9101970d44bb1571f055e60387
kylin-wong <375506637@qq.com>
2020-03-11 11:21:02 +0800

    add files

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-
+// 导入element-ui
+import './plugins/element.js'
 Vue.config.productionTip = false
 
 new Vue({
---
+++ src/plugins/element.js
@@ -0,0 +1,7 @@
+// 导入vue
+import Vue from 'vue'
+// 从element-ui中按需导入
+import {Button} from 'element-ui'
+
+// 通过use 挂载所导入的组件
+Vue.use(Button)
